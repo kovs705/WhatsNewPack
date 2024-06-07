777b03d51dd539fd5be56be52bf9f27055f8fa80
Eugene Kovs <smartkovsik@gmail.com>
2024-06-07 12:54:21 +0300

    #4 Bsaic data model

--- Sources/WhatsNewPack/Data/Feature.swift
+++ Sources/WhatsNewPack/Data/Feature.swift
@@ -14,10 +14,9 @@ public struct Feature: Codable, Hashable {
     public let new: [New]
     
     public struct New: Codable, Hashable {
-        
+        public let title: String
         public let body: String
         public let icon: String
-        public let title: String
-        public let subtitle: String
     }
+    
 }
