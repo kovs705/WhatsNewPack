9bba682ccc45e595d3643f4c2c760a7802b3e9ef
Eugene Kovs <smartkovsik@gmail.com>
2024-06-07 12:46:35 +0300

    #5 Basic WhatsNew screen init

--- Sources/WhatsNewPack/View/WhatsNew.swift
+++ Sources/WhatsNewPack/View/WhatsNew.swift
@@ -1,95 +1,19 @@
 //
 //  WhatsNew.swift
-//  Whats New
+//  WhatsNewPack
 //
-//  Created by Kovs on 06.11.2022.
+//  Created by Eugene Kovs on 07.06.2024.
+//  https://github.com/kovs705
 //
-
 
 import SwiftUI
 
-@available(iOS 14.0, *)
-public struct WhatsNew: View {
-    
-    public var featureObject: Feature
-    public var title: String = "What's new?"
-    public var color: Color  = .purple
-    
-    public var buttonTitle: String = "Continue"
-    public var buttonColor: Color  = .blue
-    public var buttonCornerRadius: CGFloat = 20
-    
-    public var action: (() -> Void)
-    
-    public init(featureObject: Feature, title: String, color: Color, buttonTitle: String, buttonColor: Color, buttonCornerRadius: CGFloat, action: @escaping () -> Void) {
-        self.featureObject = featureObject
-        self.title = title
-        self.color = color
-        self.buttonTitle = buttonTitle
-        self.buttonColor = buttonColor
-        self.buttonCornerRadius = buttonCornerRadius
-        self.action = action
-    }
-    
-    
-    public var body: some View {
-            ScrollView {
-                VStack(spacing: 70) {
-                    Spacer()
-                    
-                    VStack {
-                        Text(title)
-                            .font(.largeTitle)
-                            .bold()
-                        Text("Version \(featureObject.version)")
-                            .font(.title)
-                            .foregroundColor(.gray)
-                    }
-                    
-                    
-                    VStack(alignment: .leading, spacing: 10) {
-                        ForEach(featureObject.new, id: \.self) { newFeature in
-                            WNSubView(feature: newFeature, color: color)
-                        }
-                    }
-                    
-                    
-                    Button(action: {
-                        action()
-                    }, label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: buttonCornerRadius, style: .continuous)
-                                .frame(height: 50)
-                                .padding(.horizontal, 65)
-                            Text(buttonTitle)
-                                .font(.body)
-                                .bold()
-                                .foregroundColor(.white)
-                        }
-                    })
-                    
-                    Spacer()
-                    Spacer()
-                    // end of VStack
-                }
-            } // ScrollView
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hey, WhatsNew?!")
     }
 }
 
-@available(iOS 14.0, *)
-struct WhatsNew_Previews: PreviewProvider {
-    
-    static func printIt() {
-        print()
-    }
-    
-    static var previews: some View {
-        WhatsNew(featureObject: Feature(version: "1.0", new: [Feature.New(body: "This is a body to test how does it look like! There's also the second line if something will go wrong", icon: "chevron.left", title: "Don't worget about the title", subtitle: "Subtitle is what can help"),
-                                Feature.New(body: "This is a body to test how does it look like! There's also the second line if something will go wrong", icon: "chevron.left", title: "Don't worget about the title", subtitle: "Subtitle is what can help"),
-                                Feature.New(body: "This is a body to test how does it look like! There's also the second line if something will go wrong", icon: "chevron.left", title: "Don't worget about the title", subtitle: "Subtitle is what can help"),
-                                Feature.New(body: "This is a body to test how does it look like! There's also the second line if something will go wrong", icon: "chevron.left", title: "Don't worget about the title", subtitle: "Subtitle is what can help"),
-                                Feature.New(body: "This is a body to test how does it look like! There's also the second line if something will go wrong", icon: "chevron.left", title: "Don't worget about the title", subtitle: "Subtitle is what can help")]), title: "What's new?", color: .purple, buttonTitle: "Continue", buttonColor: .blue, buttonCornerRadius: 20) {
-            printIt()
-        }
-    }
+#Preview {
+    SwiftUIView()
 }
