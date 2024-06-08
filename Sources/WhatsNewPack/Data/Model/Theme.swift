02d7afd8b209fdc509f0596483b8f8e88be39605
Eugene Kovs <smartkovsik@gmail.com>
2024-06-08 15:13:28 +0300

    Style Model #3, Mock screen #2 Theme improve

--- Sources/WhatsNewPack/Data/Model/Theme.swift
+++ Sources/WhatsNewPack/Data/Model/Theme.swift
@@ -18,16 +18,33 @@ public struct Theme {
     public var bodyFont: Font
     public var textColor: Color
     
-    public init(backgroundColor: Color = Color(uiColor: .systemBackground),
-                iconColor: Color = .white, iconBackgroundColor: Color = .blue,
-                titleFont: Font = .body, bodyFont: Font = .subheadline,
-                textColor: Color = Color(uiColor: .label)) 
+    public var buttonBackgroundColor: Color
+    public var buttonTextColor: Color
+    public var buttonText: String
+    
+    public init
+    (
+        backgroundColor: Color = Color(uiColor: .systemBackground),
+        iconColor: Color = .white,
+        iconBackgroundColor: Color = .blue,
+        
+        titleFont: Font = .body, bodyFont: Font = .subheadline,
+        textColor: Color = Color(uiColor: .label),
+        
+        buttonBackgroundColor: Color = .blue,
+        buttonTextColor: Color = .white,
+        buttonText: String = "Cool!")
     {
         self.backgroundColor = backgroundColor
         self.iconColor = iconColor
         self.iconBackgroundColor = iconBackgroundColor
+        
         self.titleFont = titleFont
         self.bodyFont = bodyFont
         self.textColor = textColor
+        
+        self.buttonBackgroundColor = buttonBackgroundColor
+        self.buttonTextColor = buttonTextColor
+        self.buttonText = buttonText
     }
 }
